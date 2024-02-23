cdd7ae5cb159b25fd9973af339a8c09003aefa1d
AnnaSargsyanM <134032907+AnnaSargsyanM@users.noreply.github.com>
2024-02-23 10:31:36 -0600

    Add files via upload

---
+++ stats.cpp
@@ -0,0 +1,72 @@
+//****************************************************************************************************
+//     File:               stats.cpp
+// 
+//     Student:            Anna Sargsyan
+// 
+//     Assignment:         Program #8
+// 
+//     Course Name:        Programming I
+// 
+//     Course Number:      COSC 1550-01
+// 
+//     Due:                March 24, 2023
+//
+//     This program reads the scores from the file 'grades.txt'
+//     then calculates the maximum, minimum, total and average
+//     scores, as well as the number of scores.
+//     
+//     
+// 
+//****************************************************************************************************
+
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    ifstream fin;
+    fin.open("grades.txt");
+    int score;
+    int maximum = INT_MIN; 
+    int minimum = INT_MAX; 
+    int total = 0;
+    int num_scores = 0;
+
+    while (fin >> score)
+    {
+        if (score > maximum) 
+        {
+            maximum = score;
+        }
+        if (score < minimum) 
+        {
+            minimum = score;
+        }
+        total += score;
+        num_scores++;
+    }
+
+    double average = static_cast<double>(total) / num_scores;
+
+    cout << "Maximum score: " << maximum << endl;
+    cout << "Minimum score: " << minimum << endl;
+    cout << "Total score: " << total << endl;
+    cout << "Number of scores: " << num_scores << endl;
+    cout << "Average score: " << average << endl;
+
+    return 0;
+}
+
+//****************************************************************************************************
+/*
+
+Maximum score: 100
+Minimum score: 1
+Total score: 27242
+Number of scores: 374
+Average score: 72.8396
+
+*/
